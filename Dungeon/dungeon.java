2bee19734d54d695bb4ebf8da5967f800827312e
BigBoiLiam <52480219+BigBoiLiam@users.noreply.github.com>
2019-07-03 23:24:40 +1000

    general work
    candr - removed obsolete pause
    dungeon - UI improvements; commented out errors

--- Dungeon/dungeon.java
+++ Dungeon/dungeon.java
@@ -8,31 +8,56 @@ public class dungeon
 	{
 		//set up beginning of game
 		Scanner s = new Scanner(System.in);
-		System.out.println("Welcome to Alpha testing");
-		System.out.println("Do you want to enter The Dungeon?");
-		System.out.println("	[Start]	    [Quit]");
+		System.out.println("  Welcome to Alpha testing");
+		System.out.println("");
+		System.out.println("  Do you want to enter The Dungeon?");
+		System.out.println("  =================================");
+		System.out.println("  [Start]     [Quit]     [Controls]");
 		String input = "";
 		while (!input.equalsIgnoreCase("start"))
 		{
+			System.out.print("");
 			input = s.nextLine();
 			if (input.equalsIgnoreCase("start"))
 			{
-				System.out.println("You descend the stairs...");
-				delay(null);
+				System.out.println("");
+				System.out.println("  You descend the stairs...");
+				//make game wait for user
+				Delay(null);
+				//Room(room);
+				//System.out.println(room);
 			
 			}
 			else if (input.equalsIgnoreCase("quit"))
 			{
 				return;
 			}
+			else if (input.equalsIgnoreCase("controls"))
+			{
+				System.out.println("");
+				System.out.println("  To correctly select an option,");
+				System.out.println("  type exactly what is in the square");
+				System.out.println("  brackets []. E.g. [Start] type ");
+				System.out.println("  'start', or [i] type 'i'");
+				Delay(null);
+				System.out.println("");
+				System.out.println("  Do you want to enter The Dungeon?");
+				System.out.println("  =================================");
+				System.out.println("  [Start]     [Quit]     [Controls]");
+			}
 		}
-		System.out.println("just testing");
+		System.out.println("  just testing");
 		
 	}
 	
-	public static void delay(String[] args)
+	public static void Delay(String[] args)
 	{
 		Scanner s = new Scanner(System.in);
 		String delay = s.nextLine();
 	}
+
+	/*public static String Room(room)
+	{
+		String room = "test";
+	}*/
 }
