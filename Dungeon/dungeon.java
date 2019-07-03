ac0c298705550201dd85da6de1d08a0a8584f110
BigBoiLiam <52480219+BigBoiLiam@users.noreply.github.com>
2019-07-03 22:27:55 +1000

    Created basic starting game code

--- Dungeon/dungeon.java
+++ Dungeon/dungeon.java
@@ -1,22 +1,38 @@
 import java.util.*;
 import java.io.*;
+import java.lang.*;
 
 public class dungeon
 {
 	public static void main(String[] args)
 	{
+		//set up beginning of game
 		Scanner s = new Scanner(System.in);
 		System.out.println("Welcome to Alpha testing");
-		System.out.println("Do you want to enter... The Dungeon?");
-		System.out.println("	[yes]	    [no]");
-		String input = s.nextLine();
-		if (input.equals("yes"))
+		System.out.println("Do you want to enter The Dungeon?");
+		System.out.println("	[Start]	    [Quit]");
+		String input = "";
+		while (!input.equalsIgnoreCase("start"))
 		{
-			System.out.println("good on you");
+			input = s.nextLine();
+			if (input.equalsIgnoreCase("start"))
+			{
+				System.out.println("You descend the stairs...");
+				delay(null);
+			
+			}
+			else if (input.equalsIgnoreCase("quit"))
+			{
+				return;
+			}
 		}
-		else
-		{
-			System.out.println("Yeah, that's probably smart");
-		}
+		System.out.println("just testing");
+		
+	}
+	
+	public static void delay(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		String delay = s.nextLine();
 	}
 }
