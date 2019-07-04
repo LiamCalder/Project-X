8ecc76f144db47742b60352669910aeb036fd069
BigBoiLiam <52480219+BigBoiLiam@users.noreply.github.com>
2019-07-04 17:25:06 +1000

    added even more rooms
    now with 25 rooms + shopkeeper

--- Dungeon/dungeon.java
+++ Dungeon/dungeon.java
@@ -25,6 +25,7 @@ public class dungeon
                 System.out.println("  You descend the stairs...");
                 //make game wait for user
                 Delay(null);
+				//run Room method and return room String
                 String catchRoom = "";
                 String room = Room(catchRoom);
                 System.out.println(room);
@@ -42,7 +43,7 @@ public class dungeon
             {
                 return;
             }
-            else if (input.equalsIgnoreCase("controls"))
+            else
             {
                 System.out.println("");
                 System.out.println("  To correctly select an option,");
@@ -56,7 +57,6 @@ public class dungeon
                 System.out.println("  [Start]     [Quit]     [Controls]");
             }
         }
-        //after start sequence
         
     }
     
@@ -69,7 +69,7 @@ public class dungeon
     public static String Room(String room)
     {
         Random r = new Random();
-        int ran = r.nextInt(13)+1;
+        int ran = r.nextInt(31)+1;
         
         switch(ran)
         {
@@ -82,11 +82,24 @@ public class dungeon
 			case 7: room = "  An evil darkness lurks in the corners of the room"; break;
 			case 8: room = "  thick threads of spider silk coat the ceiling and walls around you"; break;
 			case 9: room = "  The floor is littered with eggs. It seems to be a nest of some sort"; break;
-			case 10: room = "  You enter an unremarable little cave, recently inhabited..."; break;
+			case 10:room = "  You enter an unremarable little cave, recently inhabited..."; break;
 			case 11:room = "  In the darkness you barely avoid falling into the underground lake in front of you"; break;
 			case 12:room = "  Bones and other, fresher, remains, lay on the floor, surrounding a dark crevice in the wall"; break;
 			case 13:room = "  Strange symbols cover all this room's surfaces"; break;
-			case 14:room = "  "; break;
+			case 14:room = "  It looks like there was once a forge here"; break;
+			case 15:room = "  rusted weapons and armour lay abandoned around you"; break;
+			case 16:room = "  The ground before you falls away into a seemingly endless abyss"; break;
+			case 17:room = "  The air around you suddenly cools"; break;
+			case 18:room = "  in front of you is a once-great statue of some forgotten hero"; break;
+			case 19:room = "  A thick mist gathers around your feet"; break;
+			case 20:room = "  You enter a mineshaft, long abandoned to rot and degradation"; break;
+			case 21:room = "  You spot a chest placed discreetly in the corner"; break; //loot chest
+			case 22:room = "  You spot a chest placed discreetly in the corner"; break; //trap chest
+			case 23:room = "  A portal to some dark world floats omniously in front of you"; break;
+			case 24:room = "  Various adventures like you lay slumped on the floor, succumbed to the perils of the dungeon"; break;
+			case 25:room = "  the doorway to this room has strange runes scrawled across it - probably a warning"; break;
+			//change max random number to change shop spawn chance (15 cases, max 20 = 1:4 spawn ratio)
+			default:room = "  A shopkeeper sits looking somewhat bored at his stall"; break;
 		}
         
         return room; 
