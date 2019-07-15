ef8e4b9e03ab9f37a56d9c91865c313d8449bbdb
BigBoiLiam <52480219+BigBoiLiam@users.noreply.github.com>
2019-07-15 16:23:23 +1000

    Delete Ranged.java

--- Dungeon - class test/Ranged.java
+++
@@ -1,9 +0,0 @@
-public class Ranged extends Weapon {
-    public Ranged(String name, double damage, int speed) {
-        super(name, damage, speed);
-    }
-
-    public int getDamage() {
-        return (int) Math.round(damage * quality); //round damage to an integer
-    }
-}
