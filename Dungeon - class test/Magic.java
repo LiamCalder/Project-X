7d55e6d53f3f502adb3e6f9d9321fad557ddca11
BigBoiLiam <52480219+BigBoiLiam@users.noreply.github.com>
2019-07-15 16:22:35 +1000

    Delete Magic.java

--- Dungeon - class test/Magic.java
+++
@@ -1,9 +0,0 @@
-public class Magic extends Weapon {
-	public Magic(String name, double damage, int speed) {
-        super(name, damage, speed);
-    }
-	
-	public int getDamage() {
-        return (int) Math.round(damage * quality); //round damage to an integer
-    }
-}
